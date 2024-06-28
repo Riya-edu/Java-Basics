d949abaeaf99a7b39ac4f312ba91294d0f7f5119
Riya-edu <edu4riya@gmail.com>
2024-06-28 21:43:09 +0530

    Pattern Programs

---
+++ Patterns/CompleteTri.java
@@ -0,0 +1,23 @@
+public class CompleteTri {
+    
+public static void main(String[] args) {
+    int n = 4;
+    int m = 5;
+
+   
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for(int i = 1; i <= m; i++){
+        for(int j = m; j>=i; j--){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+
+}
--- Patterns/InvertPyramid.java
+++ Patterns/InvertPyramid.java
@@ -1,15 +1,14 @@
-public class Pattern_09{
+public class InvertPyramid {
     public static void main(String[] args) {
-        int n = 5;
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=i; j++){
+        int n=5;
+        System.out.println();
+        for(int i=1; i <= n ; i++){
+            for(int j=1; j >= n-i; j--){
                 System.out.print(" ");
             }
-            for(int j = 1; j<=n-i; j++){
+            for(int j=n; j>= i; j--){
                 System.out.print(" * ");
             }
-            
-            
             System.out.println();
         }
     }
